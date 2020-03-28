b05615f0cc316a05581cbf5e1f4bff034269a543
Artursadikov <41513344+Artursadikov@users.noreply.github.com>
2020-03-28 22:00:19 +0300

    total price add

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -17,24 +17,10 @@ class Main extends Component {
         this.props.history.push('/');
     }
 
-    state = {
-        value: 0,
-        price: 0,
-        capsuleAmount: 1
-    }
-
-    removeItem = () => {
-        if (this.state.capsuleAmount > 1) {
-            this.setState((prevState) => ({
-                capsuleAmount: prevState.capsuleAmount - 1
-            }))
-        }
-    }
-
-    addItem = () => {
-        this.setState((prevState) => ({
-            capsuleAmount: prevState.capsuleAmount + 1
-        }))
+    componentDidMount() {
+        this.setState({
+            show: false
+        })
     }
 
     state = {
@@ -47,18 +33,81 @@ class Main extends Component {
             color5: { width: '100%' }
         },
         price: 0,
-        capsuleAmount: 1
+        totalPriceProduct: 0,
+        capsuleAmount: 1,
+        show: true
     }
+
+
+
+    removeItem = () => {
+        if (this.state.capsuleAmount > 1) {
+            this.setState((prevState) => ({
+                capsuleAmount: prevState.capsuleAmount - 1
+            }))
+        }
+    }
+
+    addItem = () => {
+        this.setState((prevState) => ({
+            capsuleAmount: prevState.capsuleAmount + 1,
+            totalPriceProduct: this.state.capsuleAmount * this.state.price,
+            show: true
+        }))
+    }
+
+
 
     handleChangeRange = (event) => {
         this.setState({
             value: event.target.value
         });
+
+        if (event.target.value === "10") {
+            this.setState({
+                price: 15,
+                totalPriceProduct: 0,
+                capsuleAmount: 1
+            })
+        } else if (event.target.value === "20") {
+            this.setState({
+                price: 22,
+                totalPriceProduct: 0,
+                capsuleAmount: 1
+            })
+        } else if (event.target.value === "30") {
+            this.setState({
+                price: 20,
+                totalPriceProduct: 0,
+                capsuleAmount: 1
+            })
+        } else if (event.target.value === "40") {
+            this.setState({
+                price: 15, totalPriceProduct: 0,
+                capsuleAmount: 1
+
+            })
+        } else if (event.target.value === "50") {
+            this.setState({
+                price: 25,
+                totalPriceProduct: 0,
+                capsuleAmount: 1
+            })
+        } else {
+            this.setState({
+                price: 0,
+                totalPriceProduct: 0,
+                capsuleAmount: 1,
+                show: false
+            })
+        }
+
+        console.log(this.state.value)
     }
 
 
 
-    progressBarHandler =() => {
+    progressBarHandler = () => {
 
         switch (this.state.value) {
             case '10':
@@ -67,26 +116,26 @@ class Main extends Component {
             case '20':
                 return <ProgressBar classnm="progress-bar progress-bar-striped bg-success progress-bar-animated"
                     color={this.state.progBarStyle.color2} />
-    
+
             case '30':
                 return <ProgressBar classnm="progress-bar progress-bar-striped bg-info progress-bar-animated"
                     color={this.state.progBarStyle.color3} />
-    
+
             case '40':
                 return <ProgressBar classnm="progress-bar progress-bar-striped  progress-bar-animated"
                     color={this.state.progBarStyle.color4} />
-    
+
             case '50':
                 return <ProgressBar classnm="progress-bar progress-bar-striped bg-danger progress-bar-animated"
                     color={this.state.progBarStyle.color5} />
-    
+
             default:
                 return <div className="coffee-str-header-div">
                     <h2 className="coffee-str-header">Choose your preferred coffee strength <br></br>---> <span className="blink">Slide Right</span></h2>
                 </div>
-    
+
         }
-    
+
     }
 
 
@@ -102,13 +151,12 @@ class Main extends Component {
                     </div>
                     <div className="imgbox row">
                         <h1 className="imgholder">IMG Placeholder</h1>
-                        <h3 className="amount">x {this.state.capsuleAmount}</h3>
+                        <div className="price-content">
+                            <h3 className="price-per-product"> {this.state.price}$ </h3>
+                            {this.state.show && this.state.value >= 10 ? <h3 className="amount">x {this.state.capsuleAmount - 1}</h3> : null}
+                        </div>
                     </div>
-                    <h3 className="price">
-                        {
-                            this.state.value
-                        }
-                    </h3>
+                    <h3 className="price"><span>Total items Price:</span> {this.state.totalPriceProduct}$ </h3>
                     <div className="coffeestrength row">
                         <input onChange={this.handleChangeRange} value={this.state.value} type="range" className="slider" min="0" max="50" step="10" />
                     </div>
