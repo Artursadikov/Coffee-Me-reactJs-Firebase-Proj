27dc4f174ace8750671143f939bb8e7e4661c947
Artursadikov <41513344+Artursadikov@users.noreply.github.com>
2020-04-03 17:36:05 +0300

    ui update

--- src/components/Cart/CartBtns.jsx
+++ src/components/Cart/CartBtns.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
 import '../Cart/CartBtnsCss.css';
+import { withRouter } from "react-router-dom";
 
-export default function CartBtns() {
-    return (
-        <div className="row order-btn">
-            <button className="res-btn">Reset</button>
-            <button className="next-btn">Next</button>
-        </div>
-    )
+class CartBtns extends Component {
+
+
+    backToMain = () => {
+        this.props.history.push('/main');
+    }
+
+    render(){
+        return (
+            <div className="row order-btn">
+                <button onClick={this.backToMain} type="button" className="res-btn">Back</button>
+                <button type="button" className="wish-li-btn">Add To Wishlist</button>
+                <button type="button" className="next-btn">Next</button>
+            </div>
+        )
+    }
+    
 }
+
+
+export default withRouter(CartBtns);
--- src/components/Cart/CartNav.jsx
+++ src/components/Cart/CartNav.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import './CartNavCss.css';
-import { Link } from 'react-router-dom';
+
 
 export default function CartNav(props) {
     return (
         <nav className='cart-nav'>
             <ul className="cart-ul">
                 <h3 className="userNameDisplay">LOGO</h3>
-                <Link to='/main' className="cart-li">Back-to-Shop</Link>
-                <Link to='/wishlist' className="cart-li">Add-to-Wishlist</Link>
+                <button type="button" className="empty-cart">Empty Cart</button>
                 <li className="your-cart">You'r Cart (Items<span>( )</span>)</li>
             </ul>
         </nav>
