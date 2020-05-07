156607cc87d391a4ba0cecdf7bd9529732b9ac95
Artursadikov <41513344+Artursadikov@users.noreply.github.com>
2020-05-07 19:57:04 +0300

    bbb

--- src/components/Sigin.js
+++ src/components/Sigin.js
@@ -18,11 +18,11 @@ class Signin extends Component {
     submitSignin = (e) => {
         e.preventDefault();
         fire.auth().signInWithEmailAndPassword(this.state.email, this.state.password).then((user) => {
-           this.props.history.push('/');
+            this.props.history.push('/');
             this.setState({
                 user: user
             })
-        }).then(()=>{
+        }).then(() => {
             axios.delete(`/Cart.json`);
         }).catch(err => {
             let errorMSG = "The password or the email is invalid !"
@@ -51,45 +51,45 @@ class Signin extends Component {
 
     render() {
         return (
-                <div className="container">
-                    <div className="divcontent">
-                        <h2 className="createheader">Login</h2>
-                        <form>
-                            <div className="form-group">
-                                <label style={{ color: 'gold', textAlign: 'center' }}>Email address</label>
-                                <input
-                                    required
-                                    value={this.state.email}
-                                    onChange={(e) => this.emailHandler(e)}
-                                    name='email' placeholder="E-mail"
-                                    type="email" className="form-control"
-                                    aria-describedby="emailHelp"
-                                />
-                            </div>
-                            <div className="form-group">
-                                <label style={{ color: 'gold', textAlign: 'center' }}>Password</label>
-                                <input
-                                    required
-                                    value={this.state.password}
-                                    onChange={(e) => this.passwordHandler(e)}
-                                    name="password" placeholder="Password"
-                                    type="password" className="form-control"
-                                />
-                            </div>
-                            <h5 className='errorMsg'>{this.state.errorMessage}</h5>
-                            <div className="buttonssingup">
-                                <Wrapper>
-                                    {this.state.email === "" || this.state.password.length < 6 ? 
-                                        <button disabled onClick={(e) => this.submitSignin(e)} type="button" style={{ backgroundColor: "transparent", border: "1px ivory solid" }} className="signupbtn">Login</button>
-                                        :
-                                        <button onClick={(e) => this.submitSignin(e)} type="button" style={{backgroundColor: 'coral', color: 'ivory'}} className="signupbtn">Login</button>
-                                    }
-                                    <button className="signupbtn google">Login With Google</button>
-                                </Wrapper>
-                            </div>
-                        </form>
-                    </div>
+            <div className="container">
+                <div className="divcontent">
+                    <h2 className="createheader">Login</h2>
+                    <form>
+                        <div className="form-group">
+                            <label style={{ color: 'gold', textAlign: 'center' }}>Email address</label>
+                            <input
+                                required
+                                value={this.state.email}
+                                onChange={(e) => this.emailHandler(e)}
+                                name='email' placeholder="E-mail"
+                                type="email" className="form-control"
+                                aria-describedby="emailHelp"
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label style={{ color: 'gold', textAlign: 'center' }}>Password</label>
+                            <input
+                                required
+                                value={this.state.password}
+                                onChange={(e) => this.passwordHandler(e)}
+                                name="password" placeholder="Password"
+                                type="password" className="form-control"
+                            />
+                        </div>
+                        <h5 className='errorMsg'>{this.state.errorMessage}</h5>
+                        <div className="buttonssingup">
+                            <Wrapper>
+                                {this.state.email === "" || this.state.password.length < 6 ?
+                                    <button disabled onClick={(e) => this.submitSignin(e)} type="button" style={{ backgroundColor: "transparent", border: "1px ivory solid" }} className="signupbtn">Login</button>
+                                    :
+                                    <button onClick={(e) => this.submitSignin(e)} type="button" style={{ backgroundColor: 'coral', color: 'ivory' }} className="signupbtn">Login</button>
+                                }
+                                {/* <button className="signupbtn google">Login With Google</button> */}
+                            </Wrapper>
+                        </div>
+                    </form>
                 </div>
+            </div>
         )
     }
 
