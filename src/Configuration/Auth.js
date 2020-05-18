ae7983097743e57c781fb743112a869bcaf4a3b7
Artursadikov <41513344+Artursadikov@users.noreply.github.com>
2020-05-18 11:02:58 +0300

    api-key-del

--- src/Configuration/Auth.js
+++ src/Configuration/Auth.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 
 let firebaseConfig = {
-    apiKey: "AIzaSyBsAFDdyVbyfamqkMMZDOZN9CYNOVeKeeo",
+    apiKey: "YOUR_API-KEY",
     authDomain: "coffe-me.firebaseapp.com",
     databaseURL: "https://coffe-me.firebaseio.com",
     projectId: "coffe-me",
     storageBucket: "coffe-me.appspot.com",
-    messagingSenderId: "1048920473475",
-    appId: "1:1048920473475:web:539acc9e0945b2f70480b6",
-    measurementId: "G-1NTV9KLXSW"
+    messagingSenderId: "*************",
+    appId: "APP-ID",
+    measurementId: "**************"
   };
 
   let fire =  firebase.initializeApp(firebaseConfig);
